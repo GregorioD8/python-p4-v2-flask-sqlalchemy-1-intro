467f3b431abd77588387fe14b92fa81b02ca207a
Gregorio <Greg.del801@gmail.com>
2024-06-09 16:35:23 -0700

    completed code-along

--- server/models.py
+++ server/models.py
@@ -1,5 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+# Defining a model with Flask-SQLAlchemy requires a Python class with four key traits:
+
+# Inheritance from the db.Model class.
+# A __tablename__ class attribute.
+# One or more class attributes assigned to the type db.Column.
+# One class attribute specified to be the table's primary key.
+
 
 # contains definitions of tables and associated schema constructs
 metadata = MetaData()
